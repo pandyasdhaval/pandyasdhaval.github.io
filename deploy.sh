a8350ca05b6839173446cb830b69282dd501fcfb
pandyasdhaval <48330813+pandyasdhaval@users.noreply.github.com>
2021-01-01 15:40:11 -0500

    Restructures deploy
    Assumptions:
      * A clone of this repository exists inside it
    
    Does:
      * ignores the clone of itself present inside it
      * ignores the 'dist' directory that build command creates
      * copies content inside the 'dist' to the clone of itself
      * adds, commits, and deploys

--- deploy.sh
+++ deploy.sh
@@ -6,18 +6,20 @@ set -e
 # build
 npm run build
 
+cp -r ./dist/* ./pandyasdhaval.github.io/
+
 # navigate into the build output directory
-cd dist
+# cd dist
+cd pandyasdhaval.github.io
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:pandyasdhaval/pandyasdhaval.github.io.git master
+git push -f git@github.com:pandyasdhaval/pandyasdhaval.github.io.git deploy
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
